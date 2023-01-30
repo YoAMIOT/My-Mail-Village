3f354d4ef785c9d7415bae0af97c3b4e641f2163
YoAMIOT <yoann.amiot.dev@gmail.com>
2023-01-30 15:22:17 +0100

    added few meshes for the future character customization and fixed the camera zoom and the walking animation of the character

--- MyMailVillageRethinked/scripts/3D/testChar.cs
+++ MyMailVillageRethinked/scripts/3D/testChar.cs
@@ -2,26 +2,25 @@ using Godot;
 using System;
 
 public class testChar : KinematicBody{
-    private float fallAcceleration = 75;
-    private int speed = 14;
+    private const float FALL_ACCELERATION = 75;
+    private const int SPEED = 7;
     private Vector3 _velocity = Vector3.Zero;
-    private float lerpValue = .15F;
-    private int zoomMin = 8;
-    private int zoomMax = 20;
-    private int zoomSpeed = 4;
+    private const float LERP_VAL = .15F;
+    private const float ZOOM_MIN = 8F;
+    private const float ZOOM_MAX = 20F;
+    private const float ZOOM_SPEED = 4F;
 
     public override void _Input(InputEvent @event){
         if (@event is InputEventMouseButton mouseEvent){
-            if (mouseEvent.IsPressed()){
+            if (mouseEvent.IsPressed() && !GetNode<Tween>("ZoomTween").IsActive()){
                 if (mouseEvent.ButtonIndex == (int)ButtonList.WheelUp){
-                    if(GetNode<SpringArm>("SpringArm").SpringLength > zoomMin){
-                        //GetNode<SpringArm>("SpringArm").SpringLength -= zoomSpeed;
-                        GetNode<Tween>("ZoomTween").InterpolateProperty(GetNode<SpringArm>("SpringArm"), "spring_length", GetNode<SpringArm>("SpringArm").SpringLength, GetNode<SpringArm>("SpringArm").SpringLength - zoomSpeed, 0.25F, Tween.TransitionType.Linear, Tween.EaseType.OutIn);
+                    if(GetNode<SpringArm>("SpringArm").SpringLength >= ZOOM_MIN + ZOOM_SPEED){
+                        GetNode<Tween>("ZoomTween").InterpolateProperty(GetNode<SpringArm>("SpringArm"), "spring_length", GetNode<SpringArm>("SpringArm").SpringLength, GetNode<SpringArm>("SpringArm").SpringLength - ZOOM_SPEED, 0.25F, Tween.TransitionType.Linear, Tween.EaseType.OutIn);
                         GetNode<Tween>("ZoomTween").Start();
                     }
                 } if (mouseEvent.ButtonIndex == (int)ButtonList.WheelDown){
-                    if(GetNode<SpringArm>("SpringArm").SpringLength < zoomMax){
-                        GetNode<Tween>("ZoomTween").InterpolateProperty(GetNode<SpringArm>("SpringArm"), "spring_length", GetNode<SpringArm>("SpringArm").SpringLength, GetNode<SpringArm>("SpringArm").SpringLength + zoomSpeed, 0.25F, Tween.TransitionType.Linear, Tween.EaseType.OutIn);
+                    if(GetNode<SpringArm>("SpringArm").SpringLength <= ZOOM_MAX - ZOOM_SPEED){
+                        GetNode<Tween>("ZoomTween").InterpolateProperty(GetNode<SpringArm>("SpringArm"), "spring_length", GetNode<SpringArm>("SpringArm").SpringLength, GetNode<SpringArm>("SpringArm").SpringLength + ZOOM_SPEED, 0.25F, Tween.TransitionType.Linear, Tween.EaseType.OutIn);
                         GetNode<Tween>("ZoomTween").Start();
                     }
                 }
@@ -30,6 +29,7 @@ public class testChar : KinematicBody{
     }
 
     public override void _PhysicsProcess(float delta){
+        GD.Print(GetNode<SpringArm>("SpringArm").SpringLength, " ", GetNode<Tween>("ZoomTween").IsActive());
         Vector3 direction = Vector3.Zero;
         if (Input.IsActionJustPressed("cameraRotateRight") && !GetNode<Tween>("CamTween").IsActive()){
             GetNode<Tween>("CamTween").InterpolateProperty(GetNode<Spatial>("SpringArm"),"rotation_degrees", GetNode<Spatial>("SpringArm").RotationDegrees, new Vector3(GetNode<Spatial>("SpringArm").RotationDegrees.x, GetNode<Spatial>("SpringArm").RotationDegrees.y + 90, GetNode<Spatial>("SpringArm").RotationDegrees.z), 0.4F, Tween.TransitionType.Sine, Tween.EaseType.InOut);
@@ -49,21 +49,22 @@ public class testChar : KinematicBody{
 
         direction = direction.Normalized();
         direction = direction.Rotated(Vector3.Up, GetNode<Spatial>("SpringArm").Rotation.y);
-        _velocity.x = direction.x * speed;
-        _velocity.z = direction.z * speed;            
-
-        if (direction != Vector3.Zero){
-            Vector3 wantedRotation = new Vector3(GetNode<Spatial>("Armature").Rotation.x, (float)Math.Atan2(_velocity.x, _velocity.z), GetNode<Spatial>("Armature").Rotation.z);
-            Vector3 currentRotation = new Vector3(GetNode<Spatial>("Armature").Rotation.x, GetNode<Spatial>("Armature").Rotation.y, GetNode<Spatial>("Armature").Rotation.z);
-            
-            if(currentRotation.y != wantedRotation.y){
-                currentRotation.y = Mathf.LerpAngle(currentRotation.y, wantedRotation.y, delta * 12);
-                GetNode<Spatial>("Armature").Rotation = currentRotation;
+        if (direction != Vector3.Zero){  
+            _velocity.x = Mathf.Lerp(_velocity.x, direction.x * SPEED, LERP_VAL);
+            _velocity.z = Mathf.Lerp(_velocity.z, direction.z * SPEED, LERP_VAL);   
+            if(GetNode<Spatial>("Armature").Rotation.y != (float)Math.Atan2(_velocity.x, _velocity.z)){
+                float newAngle = (float)Mathf.LerpAngle(GetNode<Spatial>("Armature").Rotation.y, (float)Math.Atan2(_velocity.x, _velocity.z), delta * 12);
+                GetNode<Spatial>("Armature").Rotation = new Vector3(GetNode<Spatial>("Armature").Rotation.x, newAngle, GetNode<Spatial>("Armature").Rotation.z);
             }
+        } else {
+            _velocity.x = Mathf.Lerp(_velocity.x, 0.0F, LERP_VAL);
+            _velocity.z = Mathf.Lerp(_velocity.z, 0.0F, LERP_VAL);  
         }
+        
+        GetNode<AnimationTree>("AnimationTree").Set("parameters/BlendSpace1D/blend_position", _velocity.Length() / SPEED);
 
         if(!IsOnFloor()){
-            _velocity.y -= fallAcceleration * delta;
+            _velocity.y -= FALL_ACCELERATION * delta;
         }
 
         _velocity = MoveAndSlide(_velocity, Vector3.Up);
