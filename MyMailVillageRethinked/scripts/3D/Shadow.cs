686599b16b3c2ece12209d350cb312677ffa5861
YoAMIOT <yoann.amiot.dev@gmail.com>
2023-02-15 12:38:29 +0100

    Shadow ennemy now follows the player correctly and can attack it

--- MyMailVillageRethinked/scripts/3D/Shadow.cs
+++ MyMailVillageRethinked/scripts/3D/Shadow.cs
@@ -2,20 +2,55 @@ using Godot;
 using System;
 
 public class Shadow : KinematicBody{
+    private bool inFOV = false;
+    private bool canAttack = true;
+    private Char player;
+    private const float SPEED = 7.2f;
+    private Vector3 velocity = Vector3.Zero;
+
     public override void _Ready(){
         GetNode<Area>("FOV").Connect("body_entered", this, "bodyEnteredFOV");
         GetNode<Area>("FOV").Connect("body_exited", this, "bodyExitedFOV");
+        GetNode<AnimationPlayer>("AnimationPlayer").Connect("animation_finished", this, "animationFinished");
+        GetNode<Timer>("AttackCooldown").Connect("timeout", this, "cooldownTimeout");
+        player = GetParent().GetParent().GetNode<Char>("Char");
+    }
+
+    public override void _PhysicsProcess(float delta){
+        if (inFOV){
+            GetNode<Spatial>("Appearance").LookAt(player.Translation, Vector3.Up);
+            velocity = Translation.DirectionTo(player.Translation) * SPEED;
+            velocity = MoveAndSlide(velocity);
+        }
+        if (Translation.DistanceTo(player.Translation) < 4 && canAttack){
+            attack();
+        }
     }
 
     private void bodyEnteredFOV(Node body){
-        if(body != this){
-            GD.Print(body);
+        if(body == player){
+            inFOV = true;
+        }
+    }
+    private void bodyExitedFOV(Node body){
+        if(body == player){
+            inFOV = false;
         }
     }
 
-    private void bodyExitedFOV(Node body){
-        if(body != this){
-            GD.Print(body);
+    private void attack(){
+        canAttack = false;
+        GetNode<Timer>("AttackCooldown").Start();
+        GetNode<AnimationPlayer>("AnimationPlayer").Play("Attack");
+    }
+
+    private void animationFinished(string animation){
+        if(animation == "Attack"){
+            GetNode<AnimationPlayer>("AnimationPlayer").Play("Idle");
         }
+    }
+
+    private void cooldownTimeout(){
+        canAttack = true;
     }
 }
